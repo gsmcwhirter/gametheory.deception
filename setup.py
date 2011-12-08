b5c1ca7e66c468010e8438e5792edfa2d48fc06f
Gregory McWhirter <greg@hallofkvasir.org>
2011-12-08 14:31:14 -0800

    Updates to a couple files

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
-__author__="gmcwhirt"
-__date__ ="$Sep 26, 2011 2:43:53 PM$"
+#!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup (
-  name = 'gametheory.deception',
-  version = '0.1',
-  packages = find_packages('src'),
-  package_dir = {
-    '': 'src',
-  },
-  namespace_packages = ['gametheory'],
-  install_requires = ["gametheory.base >= 0.1"],
-  author = 'Gregory McWhirter',
-  author_email = 'gmcwhirt@uci.edu',
-  description = 'Game theory simulations for deception research',
-  url = 'https://www.github.com/gsmcwhirter/gametheory',
-  license = 'MIT',
-  entry_points = {
-    "console_scripts": [
-        "gt.deception.replicator.sim = gametheory.deception.replicator.simulations:run",
-        "gt.deception.replicator.stats = gametheory.deception.replicator.stats:run",
-        "gt.deception.learning.sim = gametheory.deception.learning.main:run",
-        "gt.deception.learning.stats = gametheory.deception.learning.stats:run",
-        "gt.deception.replicator_comp.sim = gametheory.deception.replicator_comparison.simulations:run",
-        "gt.deception.replicator_comp.stats = gametheory.deception.replicator_comparison.stats:run",
-    ]
-  }
+    name = 'gametheory.deception',
+    version = '0.1',
+    packages = [
+        "gametheory.deception"
+    ],
+    package_dir = {
+        '': 'src',
+    },
+    requires = ["gametheory.base >= 0.1"],
+    author = 'Gregory McWhirter',
+    author_email = 'gmcwhirt@uci.edu',
+    description = 'Game theory simulations for deception research',
+    url = 'https://www.github.com/gsmcwhirter/gametheory',
+    license = 'MIT',
+    entry_points = {
+        "console_scripts": [
+            "gt.deception.replicator.sim = gametheory.deception.replicator.simulations:run",
+            "gt.deception.replicator.stats = gametheory.deception.replicator.stats:run",
+            "gt.deception.learning.sim = gametheory.deception.learning.main:run",
+            "gt.deception.learning.stats = gametheory.deception.learning.stats:run",
+            "gt.deception.replicator_comp.sim = gametheory.deception.replicator_comparison.simulations:run",
+            "gt.deception.replicator_comp.stats = gametheory.deception.replicator_comparison.stats:run",
+        ]
+    }
 )
